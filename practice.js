9be8dcc815d97ffd90c395775ddf0282c4f6dca7
dani <dani.zaghian@gmail.com>
2019-09-23 12:32:04 -0700

    solves problems in ruby

--- practice.js
+++ practice.js
@@ -1,53 +1,73 @@
-// Write a method that prints out the numbers 1 to 1000 that are divisible by 3.
-function printNumbersDivisibleByThree() {
-  var index = 1;
-  while (index <= 1000) {
-    if (index % 3 === 0) {
-      console.log(index);
-    }
-    index += 1;
-  }
-}
-printNumbersDivisibleByThree();
+// // Write a method that prints out the numbers 1 to 1000 that are divisible by 3.
+// function printNumbersDivisibleByThree() {
+//   var index = 1;
+//   while (index <= 1000) {
+//     if (index % 3 === 0) {
+//       console.log(index);
+//     }
+//     index += 1;
+//   }
+// }
+// printNumbersDivisibleByThree();
 
 
-// Write a method that accepts an array of strings and prints out every other string.
-function printEveryOtherItem(strings) {
-  var index = 0;
-  strings.forEach(function(string) {
-    if (index % 2 === 0) {
-      console.log(string);
-    }
-    index += 1;
-  });
-}
-printEveryOtherItem(["a", "b", "c", "d", "e"]);
+// // Write a method that accepts an array of strings and prints out every other string.
+// function printEveryOtherItem(strings) {
+//   var index = 0;
+//   strings.forEach(function(string) {
+//     if (index % 2 === 0) {
+//       console.log(string);
+//     }
+//     index += 1;
+//   });
+// }
+// printEveryOtherItem(["a", "b", "c", "d", "e"]);
 
 
-// Write a method that accepts an array of numbers and returns the sum.
-function computeSum(numbers) {
-  var sum = 0;
-  numbers.forEach(function(number) {
-    sum += number;
-  });
-  return sum;
-}
-console.log(computeSum([2, 4, 5]));
+// // Write a method that accepts an array of numbers and returns the sum.
+// function computeSum(numbers) {
+//   var sum = 0;
+//   numbers.forEach(function(number) {
+//     sum += number;
+//   });
+//   return sum;
+// }
+// console.log(computeSum([2, 4, 5]));
 
-// Start with the hash: city_populations = {chi: 2700000}
-// Add populations to the city_populations hash for New York City (8.4 million) and San Francisco (800,000).
-// The result should be: {chi: 2700000, nyc: 8400000, sf: 800000}
-var cityPopulations = {chi: 2700000};
-cityPopulations["nyc"] = 8400000;
-cityPopulations.sf = 800000;  // same as cityPopulations["sf"]
-console.log(cityPopulations);
-
-
-
-// Write a method that prints out every number from 1 to 100. 
+// // Start with the hash: city_populations = {chi: 2700000}
+// // Add populations to the city_populations hash for New York City (8.4 million) and San Francisco (800,000).
+// // The result should be: {chi: 2700000, nyc: 8400000, sf: 800000}
+// var cityPopulations = {chi: 2700000};
+// cityPopulations["nyc"] = 8400000;
+// cityPopulations.sf = 800000;  // same as cityPopulations["sf"]
+// console.log(cityPopulations);
 
 
-// Write a method that prints out every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
+
+// // Write a method that prints out every number from 1 to 100. 
+// function oneToAHundred() {
+//   var num = 0;
+//   while (num < 100) {
+//     num += 1;
+//     console.log(num);
+//   }
+// }
+
+// oneToAHundred();
+
+
+// // Write a method that prints out every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
+// function everyOtherNumber() {
+//   var num = 0;
+//   while (num < 100) {
+//     if (num % 2 !== 0) {
+//       console.log(num);
+//     }
+//     num += 1;
+//   }
+// }
+
+// everyOtherNumber();
 
 
 // Write a method that accepts an array of numbers as a parameter, and counts how many 55’s there are in the array.
--- practice.rb
+++ practice.rb
@@ -1,76 +1,149 @@
-# Write a method that prints out the numbers 1 to 1000 that are divisible by 3.
-def print_numbers_divisible_by_three
-  index = 1
-  while index <= 1000
-    if index % 3 == 0
-      puts index
-    end
-    index += 1
-  end
-end
-print_numbers_divisible_by_three
+# # Write a method that prints out the numbers 1 to 1000 that are divisible by 3.
+# def print_numbers_divisible_by_three
+#   index = 1
+#   while index <= 1000
+#     if index % 3 == 0
+#       puts index
+#     end
+#     index += 1
+#   end
+# end
+# print_numbers_divisible_by_three
 
 
-# Write a method that accepts an array of strings and prints out every other string.
-def print_every_other_item(strings)
-  index = 0
-  strings.each do |string|
-    if index % 2 == 0
-      puts string
-    end
-    index += 1
-  end
-end
-print_every_other_item(["a", "b", "c", "d", "e"])
+# # Write a method that accepts an array of strings and prints out every other string.
+# def print_every_other_item(strings)
+#   index = 0
+#   strings.each do |string|
+#     if index % 2 == 0
+#       puts string
+#     end
+#     index += 1
+#   end
+# end
+# print_every_other_item(["a", "b", "c", "d", "e"])
 
 
-# Write a method that accepts an array of numbers and returns the sum.
-def compute_sum(numbers)
-  sum = 0
-  numbers.each do |number|
-    sum += number
-  end
-  return sum
-end
-puts compute_sum([2, 4, 5])
+# # Write a method that accepts an array of numbers and returns the sum.
+# def compute_sum(numbers)
+#   sum = 0
+#   numbers.each do |number|
+#     sum += number
+#   end
+#   return sum
+# end
+# puts compute_sum([2, 4, 5])
 
 
-# Start with the hash: city_populations = {chi: 2700000}
-# Add populations to the city_populations hash for New York City (8.4 million) and San Francisco (800,000).
-# The result should be: {chi: 2700000, nyc: 8400000, sf: 800000}
-city_populations = {chi: 2700000}
-city_populations[:nyc] = 8400000
-city_populations[:sf] = 800000
-p city_populations
+# # Start with the hash: city_populations = {chi: 2700000}
+# # Add populations to the city_populations hash for New York City (8.4 million) and San Francisco (800,000).
+# # The result should be: {chi: 2700000, nyc: 8400000, sf: 800000}
+# city_populations = {chi: 2700000}
+# city_populations[:nyc] = 8400000
+# city_populations[:sf] = 800000
+# p city_populations
 
 
 
-# Write a method that prints out every number from 1 to 100. 
+# # Write a method that prints out every number from 1 to 100. 
+# def one_to_a_hundred
+#   num = 0
+#   while num < 100
+#     num += 1
+#     puts num
+#   end
+# end
+
+# one_to_a_hundred
 
 
-# Write a method that prints out every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
+# # Write a method that prints out every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
+# def every_other_number
+#   num = 0 
+#   while num < 100
+#     if num % 2 != 0 
+#       puts num
+#     end
+#     num += 1
+#   end  
+# end
+
+# every_other_number
 
 
-# Write a method that accepts an array of numbers as a parameter, and counts how many 55’s there are in the array.
+# # Write a method that accepts an array of numbers as a parameter, and counts how many 55’s there are in the array.
+# def num_of_55(numbers)
+#   count = 0
+#   numbers.each do |num|
+#     if num == 55
+#       count += 1
+#     end
+#   end
+#   puts count
+# end
+
+# num_of_55([1, 55, 55, 1002, 2, 55])
 
 
-# Write a method that accepts an array of strings and returns a new array that has the string "awesomesauce" inserted between every string. 
-# For example, if the initial array is ["a", "b", "c", "d", "e"], then the returned array should be ["a", "awesomesauce", "b", "awesomesauce", "c", "awesomesauce", "d", "awesomesauce", "e"].
+# # Write a method that accepts an array of strings and returns a new array that has the string "awesomesauce" inserted between every string. 
+# # For example, if the initial array is ["a", "b", "c", "d", "e"], then the returned array should be ["a", "awesomesauce", "b", "awesomesauce", "c", "awesomesauce", "d", "awesomesauce", "e"].
+
+# def awesomesauce(strings)
+#   length_of_array = strings.length
+#   index = 0
+#   awesomesauced_array = []
+
+#   while index < length_of_array
+#     awesomesauced_array << strings[index]
+#     if index != length_of_array - 1
+#       awesomesauced_array << "awesomesauce"
+#     end
+#     index += 1
+#   end
+#   awesomesauced_array
+# end
+
+# p awesomesauce(["a", "b", "c", "d", "e"])
+
+# # Start with the hash: item_amounts = {chair: 5, table: 2}
+# # Someone just bought two chairs. Change the hash such that the chair amount is 3.
+# # The final result should be: {chair: 3, table: 2}
+# item_amounts = {chair: 5, table: 2}
+# item_amounts[:chair] -= 2
+# p item_amounts
 
 
-# Start with the hash: item_amounts = {chair: 5, table: 2}
-# Someone just bought two chairs. Change the hash such that the chair amount is 3.
-# The final result should be: {chair: 3, table: 2}
+# # Start with the hash: item_amounts = {chair: 5, table: 2}
+# # You received 7 desks to sell. Change the hash to include desks.
+# # The final result should be: {chair: 5, table: 2, desk: 7}
+# item_amounts = {chair: 5, table: 2}
+# item_amounts[:desks] = 7
+# p item_amounts
 
 
-# Start with the hash: item_amounts = {chair: 5, table: 2}
-# You received 7 desks to sell. Change the hash to include desks.
-# The final result should be: {chair: 5, table: 2, desk: 7}
+# # Write a method that accepts a number and returns its factorial.
+# # For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+# def factorial(num)
+#   product = 1
+#   while num > 0
+#     product *= num
+#     num -= 1
+#   end
+#   product  
+# end
+
+# puts factorial(6)
 
 
-# Write a method that accepts a number and returns its factorial.
-# For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+# # Write a method that accepts two arrays of numbers, and prints the sum of every combination of numbers from first and second array. 
+# # For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should print a list: 101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
 
+# def print_sums(nums_1, nums_2)
+#   nums_1.each do |num_1|
+#     nums_2.each do |num_2|
+#       puts num_1 + num_2
+#     end
+#   end
+# end
 
-# Write a method that accepts two arrays of numbers, and prints the sum of every combination of numbers from first and second array. 
-# For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should print a list: 101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+# print_sums([1, 5, 10], [100, 500, 1000])
